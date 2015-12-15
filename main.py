41770066874f001201040a48fd31912e0a1b71e1
Zhiyi Luo <jessieluo1991@gmail.com>
2015-12-15 19:59:20 +0800

    mnist processor

---
+++ main.py
@@ -0,0 +1,18 @@
+import configparser
+import mnistProcessor
+
+def train(configfilePath):
+    pass
+
+if __name__=="__main__":
+    configfilePath="mnist-config.ini"
+    config = configparser.ConfigParser(interpolation=configparser.ExtendedInterpolation()) # The interpolation parameter is important,
+                                # this makes the `.ini` config file can use the `${}` syntax.
+    config.read(configfilePath)
+    #print config.keys() # config is sort of a dict
+    #print config.get('DEFAULT','train_images_file')
+    processor = mnistProcessor.mnistProcessor()
+    train_data = processor.loadMNISTimages(config.get('DEFAULT','train_images_file'))
+    train_label = processor.loadLabels(config.get('DEFAULT','train_labels_file'))
+    test_data = processor.loadMNISTimages(config.get('DEFAULT','test_images_file'))
+    test_label = processor.loadLabels(config.get('DEFAULT','test_labels_file'))
---
+++ mnistProcessor.py
@@ -0,0 +1,48 @@
+import os, struct
+import numpy as np
+#import gnumpy as gp
+import matplotlib.pyplot as plt
+
+class mnistProcessor(object):
+    """ processing functionality for mnist images """
+
+    def __init__(self):
+        super(mnistProcessor,self).__init__()
+
+    def loadMNISTimages(self, filename):
+        """ mnist images files are binary files.
+        Read them into numpy(or gnumpy) arrays
+        """
+        binf = open(filename, 'rb')
+        # `>IIII` means read 4 unsigned int32 as `>` code way, i.e. 4*32 bits = 16 Bytes,
+        # so the second param is `binf.read(16)`
+        magic_nr, size, rows, cols = struct.unpack(">IIII",binf.read(16))
+
+        # We use `numpy.uint8` 2D array to store image pixels,
+        # since intensity of image pixels range from 0 to 255 (2^8 = 256).
+        data = np.empty([size, rows, cols], dtype=np.uint8) # so one pixel is one byte
+        perimbytes = rows*cols
+        for i in range(size):
+            im = np.array(struct.unpack(">"+str(perimbytes)+"B", binf.read(perimbytes))).reshape(rows, cols)
+            data[i] = im
+        binf.close()
+
+        """
+        #im = struct.unpack(">784B",binf.read(28*28))
+        #im = np.array(im).reshape(28,28)
+        im = data[0]
+        # print im.shape
+        plt.imshow(im, cmap='gray')
+        plt.show()
+        """
+        print data.shape
+        return data
+
+    def loadLabels(self, filename):
+        binf = open(filename, 'rb')
+        magic_nr, size = struct.unpack(">II", binf.read(8))
+        data = np.empty([size,1], dtype = np.int8) # label range from 0 to 9
+        for i in range(size):
+            data[i] = np.array(struct.unpack(">1B", binf.read(1)))
+        binf.close()
+        # print type(data[0]), data[0].shape, data[0]
